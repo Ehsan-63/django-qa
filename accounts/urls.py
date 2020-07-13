395b3df45fefd74fc891016af5bb7ae04ced5ee0
amirbigg <amirbig44@gmail.com>
2020-07-13 11:16:13 +0430

    add ep3

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
 	path('register/', views.UserRegister.as_view(), name='register'),
+	path('login/', views.UserLogin.as_view(), name='login'),
+	path('logout/', views.UserLogout.as_view(), name='logout'),
 ]
