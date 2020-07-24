81834fc86040d3070179f205f9fe557739306ba4
amirbigg <amirbig44@gmail.com>
2020-07-24 23:10:12 +0430

    add ep6

--- core/views.py
+++ core/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Question
-
+from .forms import SearchForm
+from django.db.models import Q
 
 
 class Home(View):
+	form_class = SearchForm
+
 	def get(self, request):
+		form = self.form_class
 		questions = Question.objects.all()
-		return render(request, 'core/home.html', {'questions':questions})
+		search = request.GET.get('search')
+		if search:
+			questions = questions.filter( Q(title__icontains=search) | Q(body__icontains=search) )
+		return render(request, 'core/home.html', {'questions':questions, 'form':form})
 
 
 class QuestionDetail(View):
