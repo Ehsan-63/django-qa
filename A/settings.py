e883ad0ef3d6b4b4ba79693b872ee6ded94c8b0b
amirbigg <amirbig44@gmail.com>
2020-07-27 14:24:51 +0430

    add ep9

--- A/settings.py
+++ A/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'accounts.apps.AccountsConfig',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Question
 from .forms import SearchForm
-from django.db.models import Q
+from django.contrib.postgres.search import SearchVector
 
 
 class Home(View):
@@ -11,9 +11,11 @@ class Home(View):
 	def get(self, request):
 		form = self.form_class
 		questions = Question.objects.all()
-		search = request.GET.get('search')
-		if search:
-			questions = questions.filter( Q(title__icontains=search) | Q(body__icontains=search) )
+		if 'search' in request.GET:
+			form = self.form_class(request.GET)
+			if form.is_valid():
+				query = form.cleaned_data['search']
+				questions = questions.annotate(search=SearchVector('title', 'body')).filter(search=query)
 		return render(request, 'core/home.html', {'questions':questions, 'form':form})
 
 
