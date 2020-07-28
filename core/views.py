b9e5bcfeb2e47806dc1254e5c6cfc0298f0decbf
amirbigg <amirbig44@gmail.com>
2020-07-28 12:10:53 +0430

    add ep10

--- core/views.py
+++ core/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Question
 from .forms import SearchForm
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import TrigramSimilarity
+from django.db.models.functions import Greatest
 
 
 class Home(View):
@@ -15,7 +16,12 @@ class Home(View):
 			form = self.form_class(request.GET)
 			if form.is_valid():
 				query = form.cleaned_data['search']
-				questions = questions.annotate(search=SearchVector('title', 'body')).filter(search=query)
+				questions = questions.annotate(
+						similarity = Greatest(
+							TrigramSimilarity('title', query),
+							TrigramSimilarity('body', query)
+						)
+				).filter(similarity__gt=0).order_by('-similarity')
 		return render(request, 'core/home.html', {'questions':questions, 'form':form})
 
 
